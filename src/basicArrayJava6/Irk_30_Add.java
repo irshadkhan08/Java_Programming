24635e1612651e6d8a201f464467233fddf3e791
irshadkhan08 <irshadkhan@Irshads-MacBook-Air.local>
2022-08-08 08:26:26 +0530

    sum of two number

---
+++ src/basicArrayJava6/Irk_30_Add.java
@@ -0,0 +1,9 @@
+package basicArrayJava6;
+
+public class Irk_30_Add {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        System.out.println(a+b);
+    }
+}
